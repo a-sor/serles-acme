fb048271858fa818f4df20f3788f14913aa48ab5
Alexander Sorokin <sorockin@yandex.ru>
2024-04-23 19:56:50 +0300

    Remove init_config()
    The function init_config() has been removed, all appropriate adjustments have
    been made, including tests. Config and backend are now globally accessed as
    serles.config and serles.backend correspondingly.

--- serles/views.py
+++ serles/views.py
@@ -8,7 +8,6 @@ from flask_restful import Resource, Api
 from .utils import base64d
 from .models import *
 from .challenge import (
-    init_config as c_init_config,
     verify_challenge,
     check_csr_and_return_cert,
 )
@@ -17,10 +16,6 @@ from .exceptions import ACMEError
 from datetime import datetime, timedelta
 
 api = Api()
-
-
-def init_config():
-    c_init_config()
 
 
 @api.resource("/")
